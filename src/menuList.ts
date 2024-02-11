beca2edb7831cfede99bf21ec85ef6dc881a1f26
dotera <dotera.s@tam-tam.co.jp>
2024-02-11 18:03:02 +0900

    topページ移行

---
+++ src/menuList.ts
@@ -0,0 +1,28 @@
+export const menuList = [
+  {
+    name: "About",
+    path: "/about/",
+  },
+  {
+    name: "Program",
+    path: "/program/",
+  },
+  {
+    name: "Abstract Submission",
+    path: "/abstract/",
+    forHeader: "Abstract <br/>Submission",
+  },
+  {
+    name: "Registration & Accomodation",
+    path: "/registration/",
+    forHeader: "Registration & <br/>Accomodation",
+  },
+  {
+    name: "Venue",
+    path: "/venue/",
+  },
+  {
+    name: "Photos",
+    path: "/photos/",
+  },
+];
